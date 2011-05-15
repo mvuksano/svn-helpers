beb0f47aa49ccb975df98a9ecf7baea137e05f43
Marko Vukasnovic <markovuksanovic@gmail.com>
2011-05-15 11:38:57 +0200

    removing sample executable file

--- mycheckout.bat
+++
@@ -1,4 +0,0 @@
-start svncheckout.py ericsson-mhealth.txt https://rdsrv005.design.etk.extern.eu.ericsson.se/svnrepos/svnrep-ericsson-mhealth/trunk/
-start svncheckout.py ericsson-mhealth-etmc.txt https://rdsrv005.design.etk.extern.eu.ericsson.se/svnrepos/svnrep-ericsson-mhealth-etmc/trunk/
-start svncheckout.py ericsson-mhealth-mn.txt https://rdsrv005.design.etk.extern.eu.ericsson.se/svnrepos/svnrep-ericsson-mhealth-mn/trunk/
-start svncheckout.py ericsson-mhealth-products.txt https://rdsrv005.design.etk.extern.eu.ericsson.se/svnrepos/svnrep-ericsson-mhealth-products/trunk/
